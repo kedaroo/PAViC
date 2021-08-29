a87116209ef105f6809af8bd9943be6a81de8dca
kedaroo <krb1202@gmail.com>
2021-08-30 02:23:55 +0530

    works

--- screens/RootStackScreen.js
+++ screens/RootStackScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -6,16 +6,50 @@ import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './SplashScreen';
 import SignInScreen from './SignInScreen2';
 import SignUpScreen from './SignUpScreen';
+import OnboardingScreen from './OnboardingScreen';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const RootStack = createStackNavigator();
 
-const RootStackScreen = ({ navigation }) => (
-    <RootStack.Navigator screenOptions={{headerShown:false}}>
-        <RootStack.Screen name='SplashScreen' component={SplashScreen} />
-        <RootStack.Screen name='SignInScreen' component={SignInScreen} />
-        <RootStack.Screen name='SignUpScreen' component={SignUpScreen} />
-    </RootStack.Navigator>
-)
+const RootStackScreen = ({ navigation }) => {
+    const [isFirstLaunch, setIsFirstLaunch] = useState(null)
+
+    useEffect(() => {
+        AsyncStorage.getItem('alreadyLaunchedd').then(value => {
+            if(value == null) {
+                AsyncStorage.setItem('alreadyLaunched', 'true')
+                setIsFirstLaunch(true)
+            } else {
+                setIsFirstLaunch(false)
+            }
+        })
+    }, [])
+
+    if (isFirstLaunch == null) {
+        return null
+    } else if (isFirstLaunch == true) {
+        return (
+            <RootStack.Navigator screenOptions={{headerShown:false}}>
+                <RootStack.Screen name='Onboarding' component={OnboardingScreen} />
+                <RootStack.Screen name='SplashScreen' component={SplashScreen} />
+                <RootStack.Screen name='SignInScreen' component={SignInScreen} />
+                <RootStack.Screen name='SignUpScreen' component={SignUpScreen} />
+            </RootStack.Navigator>
+        )
+    } else {
+        return <SplashScreen />
+    }
+
+    // return(
+    //     <RootStack.Navigator screenOptions={{headerShown:false}}>
+    //         <RootStack.Screen name='Onboarding' component={OnboardingScreen} />
+    //         <RootStack.Screen name='SplashScreen' component={SplashScreen} />
+    //         <RootStack.Screen name='SignInScreen' component={SignInScreen} />
+    //         <RootStack.Screen name='SignUpScreen' component={SignUpScreen} />
+    //     </RootStack.Navigator>
+    // )
+}
 
 export default RootStackScreen;
