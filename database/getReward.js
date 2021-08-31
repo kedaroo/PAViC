15e8643e2f84fb028ba3c7dfa89216afa39158b4
kedaroo <krb1202@gmail.com>
2021-08-31 14:08:09 +0530

    get reward refactor

--- database/getReward.js
+++ database/getReward.js
@@ -5,19 +5,13 @@ function from(username) {
     return new Promise(function(resolve, reject) {
         db.transaction((tx) => {
             tx.executeSql(
-                // 'SELECT SUM(amount) FROM transactions where from_add = 0',
                 'select sum(amount) from transactions where from_add = "Reward" and to_add = ?',
                 [username], 
                 (_tx, {rows: {_array} }) => {
-                    console.log('THIS IS THE FROM IN fetch reward')
-                    // console.log(_array[0]['SUM(amount)'])
-                    console.log(_array[0]['sum(amount)'])
                     var result = _array[0]['sum(amount)']
                     if (result == null) {
-                        console.log('REWARD FETCHED AS FOLLOWS:: ')
                         resolve(0)
                     } else {
-                        console.log('REWARD FETCHED AS FOLLOWS:: ')
                         resolve(_array[0]['sum(amount)'])
                     }
                 }, 
@@ -41,10 +35,6 @@ const getReward = () => {
     return new Promise( async function(resolve, reject) {
         var username = await fetchUserName()
         var reward = await from(username)
-        // console.log('THIS IS INSIDE getBalance. THIS IS BALANCE BELOW')
-        console.log('7.3 REWARD FETCHED AS FOLLOWS::')
-        console.log(reward)
-        // console.log('THIS IS BALANCE TYPE: ', typeof balance)
         resolve(reward)
     })
 }
