e884d95d01f1f547150f9b20ca37c83ddb6f5b49
kedaroo <krb1202@gmail.com>
2021-08-31 14:20:31 +0530

    rootstack refactor

--- screens/RootStackScreen.js
+++ screens/RootStackScreen.js
@@ -2,14 +2,10 @@ import React, {useState, useEffect} from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 
-// import SignInScreen from './userNameScreen';
 import SplashScreen from './SplashScreen';
-import SignInScreen from './SignInScreen2';
-import SignUpScreen from './SignUpScreen';
 import OnboardingScreen from './OnboardingScreen';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
 
 const RootStack = createStackNavigator();
 
@@ -34,22 +30,11 @@ const RootStackScreen = ({ navigation }) => {
             <RootStack.Navigator screenOptions={{headerShown:false}}>
                 <RootStack.Screen name='Onboarding' component={OnboardingScreen} />
                 <RootStack.Screen name='SplashScreen' component={SplashScreen} />
-                {/* <RootStack.Screen name='SignInScreen' component={SignInScreen} /> */}
-                {/* <RootStack.Screen name='SignUpScreen' component={SignUpScreen} /> */}
             </RootStack.Navigator>
         )
     } else {
         return <SplashScreen />
     }
-
-    // return(
-    //     <RootStack.Navigator screenOptions={{headerShown:false}}>
-    //         <RootStack.Screen name='Onboarding' component={OnboardingScreen} />
-    //         <RootStack.Screen name='SplashScreen' component={SplashScreen} />
-    //         <RootStack.Screen name='SignInScreen' component={SignInScreen} />
-    //         <RootStack.Screen name='SignUpScreen' component={SignUpScreen} />
-    //     </RootStack.Navigator>
-    // )
 }
 
 export default RootStackScreen;
