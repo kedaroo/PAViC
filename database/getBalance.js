26fb973b6689704f01b2a17eaf88faa9160113af
kedaroo <krb1202@gmail.com>
2021-08-31 14:06:27 +0530

    get balace function refactor

--- database/getBalance.js
+++ database/getBalance.js
@@ -9,14 +9,10 @@ function from(username) {
                 'SELECT SUM(amount) FROM transactions where from_add = ?',
                 [username], 
                 (_tx, {rows: {_array} }) => {
-                    // console.log('THIS IS THE FROM')
-                    // console.log(_array[0]['SUM(amount)'])
                     var result = _array[0]['SUM(amount)']
                     if (result == null) {
-                        console.log('FROM AMOUNT::')
                         resolve(0)
                     } else {
-                        console.log('FROM AMOUNT::')
                         resolve(_array[0]['SUM(amount)'])
                     }
                 }, 
@@ -33,14 +29,10 @@ function to(username) {
                 'SELECT SUM(amount) FROM transactions where to_add = ?',
                 [username], 
                 (_tx, {rows: {_array} }) => {
-                    // console.log('THIS IS THE TO')
-                    // console.log(_array[0]['SUM(amount)'])
                     var result = _array[0]['SUM(amount)']
                     if (result == null) {
-                        console.log('TO AMOUNT::')
                         resolve(0)
                     } else {
-                        console.log('TO AMOUNT::')
                         resolve(_array[0]['SUM(amount)'])
                     }
                 }, 
@@ -53,10 +45,8 @@ function to(username) {
 function fetchUserName() {
     return new Promise(async function(resolve, reject) {
         const token = await AsyncStorage.getItem('userToken')
-        console.log('This is inside fetch username. sub sent::', JSON.parse(token).sub)
         socket.emit("fetch username", JSON.parse(token).sub)
         socket.once("get username", args => {
-            console.log('This is inside fetch username. username received::', args)
             resolve(args)
         })
     })
@@ -68,10 +58,6 @@ const getBalance = () => {
         var from_amt = await from(username)
         var to_amt = await to(username)
         var balance = to_amt - from_amt
-        // console.log('THIS IS INSIDE getBalance. THIS IS BALANCE BELOW')
-        console.log('7.3 BALANCE FETCHED AS FOLLOWS::')
-        console.log(balance)
-        // console.log('THIS IS BALANCE TYPE: ', typeof balance)
         resolve(balance)
     })
 }
