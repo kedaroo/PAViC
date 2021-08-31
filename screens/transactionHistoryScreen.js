24afa941f0fb653421d826ba89dfb5394706cad1
kedaroo <krb1202@gmail.com>
2021-08-31 14:54:23 +0530

    transaction history screen refactor

--- screens/transactionHistoryScreen.js
+++ screens/transactionHistoryScreen.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet, RefreshControl, useCallback, Image, Dimensions } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Dimensions } from 'react-native';
 import db from '../database/database';
 import TransactionCard from '../shared/transactionCard';
 
 export default function TransactionHistoryScreen ({ route }) {  
-
-    // const mobile = route.params.mobile
 
     const sub = JSON.parse(route.params.token).sub
     
@@ -18,40 +16,15 @@ export default function TransactionHistoryScreen ({ route }) {
 
     const [transactions, setTransactions] = useState(null);
     const [refreshing, setRefreshing] = useState(false);
-    const [users, setUsers] = useState()
-
-    // const loadUsers = () => {
-    //     db.transaction((tx) => {
-    //         console.log('TRYING TO LOAD USERS FOR HistoryScreen...')
-    //         tx.executeSql(
-    //             'SELECT name, mobile FROM users',
-    //             [], 
-    //             (_tx, {rows: {_array} }) => {
-    //                 console.log('LOADED USERS:')
-    //                 var dict = {}
-    //                 for (var i = 0; i < _array.length; i++) {
-    //                     dict[_array[i].mobile] = _array[i].name 
-    //                 }
-    //                 setUsers(dict)
-    //                 console.log(users)
-    //             }, 
-    //             () => console.log('Fetching USERS FOR HISTORY FAILED!')
-    //         )
-    //     }, () => console.log('Fetching USERS FOR HISTORY error'), () => console.log('Fetching USERS FOR HISTORY SUCCESSFULL'));
-    // }
-
 
     const refreshTransactions = () => {
         setRefreshing(true);
         db.transaction((tx) => {
-            console.log('TRYING TO LOAD TRANSACTION FOR HistoryScreen...')
             tx.executeSql(
                 'SELECT * FROM transactions where from_add = ? or to_add = ?',
                 [userName, userName], 
                 (_tx, {rows: {_array} }) => {
-                    console.log('LOADED TRANSACTIONS:')
                     setTransactions(_array.reverse())
-                    console.log(transactions)
                 }, 
                 () => console.log('Fetching TRANSACTION FOR HISTORY FAILED!')
             )
@@ -59,39 +32,13 @@ export default function TransactionHistoryScreen ({ route }) {
         setRefreshing(false)
       }
 
-      const loadTransactions = () => {
-        db.transaction((tx) => {
-            console.log('TRYING TO LOAD TRANSACTION FOR HistoryScreen...')
-            tx.executeSql(
-                'SELECT * FROM transactions where from_add = ? or to_add = ?',
-                [userName, userName], 
-                (_tx, {rows: {_array} }) => {
-                    console.log('LOADED TRANSACTIONS:')
-                    setTransactions(_array.reverse())
-                    console.log(transactions)
-                }, 
-                () => console.log('Fetching TRANSACTION FOR HISTORY FAILED!')
-            )
-        }, () => console.log('Fetching TRANSACTION FOR HISTORY error'), () => console.log('Fetching TRANSACTION FOR HISTORY SUCCESSFULL'));
-    }
-
     useEffect(() => {
-
-        // loadUsers()
-        // loadTransactions()
-        
-
-        // socket.on("add new transactions", transactions => {
-        //     console.log('This is inside add new transactions event listner in TRANSACTION HISTORY SCREEN')
-        //     loadTransactions()
-        // })        
 
     }, [])
 
     return (
         <View style = {styles.container} >
 
-            {/* <Text style={styles.title}>Recent Transactions</Text> */}
             <View style={{padding: 10, marginTop: 10}}>
                 <Text style={{color: '#374151', fontSize: 24, fontWeight: '700'}}>Recent</Text>
                 <Text style={{color: '#374151', fontSize: 38, fontWeight: '700'}}>Transactions</Text>
@@ -100,10 +47,6 @@ export default function TransactionHistoryScreen ({ route }) {
             
         
             <View style={{flex:1}}>
-                {/* <Image 
-                    source={require('../assets/empty.png')} 
-                    style={styles.image}
-                />  */}
                 <FlatList 
                     data={transactions}
                     renderItem={({item}) => (
@@ -113,8 +56,6 @@ export default function TransactionHistoryScreen ({ route }) {
                     refreshing={refreshing}
                 />
             </View>
-            
-            
         </View>
     )
 }
@@ -126,8 +67,6 @@ const styles = StyleSheet.create({
         padding: 10,
         paddingTop: 20,
         paddingBottom: 100,
-        // backgroundColor: '#F9FAFB'
-        // backgroundColor: 'green'
     },  
     title: {
       fontSize: 28,
@@ -136,13 +75,11 @@ const styles = StyleSheet.create({
       marginHorizontal: 10
     },
     imageContainer: {
-        // backgroundColor: 'pink',
         flex:1,
         justifyContent: 'space-around',
         alignItems: 'center'
     },
     image: {
-        // backgroundColor: 'yellow',
         resizeMode: 'contain',
         padding: 0,
         flex:1,
@@ -152,8 +89,5 @@ const styles = StyleSheet.create({
     message :{
         color: 'gray',
         fontSize: 20,
-        // marginTop: -50
-        // marginBottom: 50
-
     }
   });
